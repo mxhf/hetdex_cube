59ada6db01d8279ca1874755c40a4d6c60720f13
Maximilian Fabricius <mxhf@login3.stampede2.tacc.utexas.edu>
2019-09-05 03:30:57 -0500

    Simplified global sky camputation that is compatible with rebin files from hdr1.

--- global_sky2.py
+++ global_sky2.py
@@ -1,214 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 # coding: utf-8
 from __future__ import print_function
-
-import pylab
-from astropy.io import fits
 import sys
-from scipy import *
-from scipy import optimize
-import spectrum
-import numpy
-import optparse
-import PolygonIntersect
-from matplotlib import pyplot as plt
-import spectrum
-from astropy.io import ascii
-from astropy.table import Table, Column
-from astropy import table
-import os
+import pickle
 import glob
 import numpy as np
-from scipy.interpolate import UnivariateSpline
-import srebin
-import sys
-from astropy.table import Column, vstack
+import os
+
+from astropy.io import fits
 from astropy.io import ascii
-
-def get_rebinned(hdu, extensions, start = 3494.74, step =  1.9858398, stop = 5500.):
-    #start,stop = 3503.9716796, 5396.477
-    N = int( np.ceil( (stop - start)/step ) )
-
-    rebinned = {}
-    wl = hdu['wavelength'].data
-    for ext in extensions:
-        new = np.zeros([wl.shape[0], N])
-
-        for i in range(wl.shape[0]):
-            w = wl[i,:]
-            f = hdu[ext].data[i,:]
-            start = start
-            step =  step
-            stop = stop
-            lw, lf = srebin.linlin(w, f, start, step, stop, dowarn=False)
- 
-            # hack as they may not all necessareyly have the same length
-            new[i,:min(N, len(lf))] = lf[:min(N, len(lf))]
-
-        rebinned[ext] = new
-    return lw, rebinned
-
-def read_shotlist(shotlist_file):
-    return Table(ascii.read(args.shotlist, format="fast_no_header"), names=["shots"])
+from astropy.table import Table, Column
+from astropy.table import Column, vstack
+from astropy import table
+from scipy import *
+from scipy import optimize
 
 
-def combine_dithall(shotlist, shifts_dir):
-    tables = []
-    for shot in shotlist["shots"]:
-            dithall_filename = "{}/dithall.use".format(shot)
-            filename = os.path.join(shifts_dir, dithall_filename)
-            print("Reading {}".format( filename ))
-            t = ascii.read(filename)
-            night, shotid = shot.split("v")
-            cn = Column(name="night", data=[night]*len(t) )
-            cs = Column(name="shotid", data=[shotid]*len(t) )
-            t.add_column(cs,index=0)
-            t.add_column(cn,index=0)
-            tables.append(t)
-
-    T = vstack(tables)
-    return T
+# In[2]:
 
 
+def read_shotlist(fnshotlist):
+    return Table(ascii.read(fnshotlist, format="fast_no_header"), names=["shots"])
 
 
-import argparse
-
-parser = argparse.ArgumentParser(description='Build a hetdex cube.')
-#parser.add_argument('--basepath', default="/work/03946/hetdex/maverick/red1/reductions")
-parser.add_argument('--basepath', default="../reductions")
-parser.add_argument('--shiftsdir', type=str, default="../shifts",
-                            help='Directory that contains the astrometric solutions for each shot.')
-parser.add_argument('--dither_use', type=str,
-                            help='Combined dithall use file.')
-parser.add_argument('--force_rebin', action="store_true",
-                            help='Force rebinning rather then using prior cached rebinning results.')
-parser.add_argument('--shotlist', type=str,
-                            help='List of actual shots to use.')
-args = parser.parse_args()
-
-force_rebin = args.force_rebin
-basepath = args.basepath
-prefix = ""
-extensions = ["sky_subtracted", "sky_spectrum", "fiber_to_fiber"]
+# In[12]:
 
 
-shotlist = read_shotlist(args.shotlist)
-# read dithall.use
-t = combine_dithall(shotlist, args.shiftsdir)
-
-per_amp_sky_spectra = [] # holds for each fiber spectrum
-                 # the corresponing amplifier wide sky (median accorss all fibers after correcting for fiber_to_fiber)
-wlgrid = None
-names = ["count", "amplifier", "fiberid", "ra", "dec", "shot", "night", "shotid", "exp"]
-dtype = [int, 'U2', int, float, float, 'U12', 'U8', 'U3', 'U6']
-fibers = Table(names=names, dtype=dtype)
-
-count = 0
-fid = -1
-
-import pickle
-
-###############################################################################
-# read spectra
-###############################################################################
-
-def amp(x):
-    return x[18:20]
+COMMANDLINE = True
 
 
-def multifix(path):
-    h,t = os.path.split(path)
-    #multi_008_093_054_LL.fits 
-    ifu = t[10:13]
-    amp = t[18:20]
+# In[4]:
+
+
+if COMMANDLINE:
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Build a hetdex cube.')
+    parser.add_argument('--dir_rebin', default="/scratch/04287/mxhf/rebin2")
+    parser.add_argument('--shot', type=str, default="20180110v021",
+                                help='Actual shots to use.')
+    parser.add_argument('--usepca', action="store_false", default=True,
+                                help='Do use PCA sky subtracted data (True).')
+    args = parser.parse_args()
+
+    dir_rebin = args.dir_rebin
     
-    pattern = os.path.join( h,"multi_???_{}_???_{}.fits".format(ifu,amp) )
-    ff = glob.glob(pattern)
-    if not len(ff) == 1:
-        #print("Error, unable to identify correct multifits for ifu {} and amp {} in {}".format(ifu, amp, h))
-        return path
-    else:
-        return ff[0]
+    shot = args.shot
+    
+    usepca = args.usepca
+else:
+    
+    dir_rebin = "/scratch/04287/mxhf/rebin2"
+    shot = "20180110v021"
+    usepca = True
 
 
-camp = map(amp, t["multifits"]) 
-
-camp = list(camp)
+# In[8]:
 
 
-t.add_column(Column(camp, name='amp') )
-ut1 = table.unique(t, keys=['night', 'shotid', 'exposure', 'ifuslot', 'amp'])
-ut1.add_column(Column(ut1['night'] == "0", name='exists', dtype=bool) )
+# find all files for all shots
+if usepca:
+    prefix = "pca_"
+else:
+    prefix = ""
 
-print("len(ut1) = ", len(ut1) )
+
+pattern = os.path.join(dir_rebin, shot, "exp0?", "{}multi_xxx_???_xxx_??_rebin.pickle".format(prefix))
+print("Finding rebinned data for {}\n (searching for {})".format(shot, pattern))
+shotfiles = glob.glob( pattern )
+print("Found {} files.".format(len(shotfiles)))
 
 
-for r in ut1:
-    mf = prefix + r["multifits"]
-    print(mf)
+# In[9]:
 
-    exp = r["exposure"]
-    night = r["night"]
-    shotid = "{}".format( r["shotid"] )
-    shot =  "{}v{}".format(night,shotid)
-    if (not (shot in shotlist["shots"])):
-        continue
 
-    amp = mf[-10:-8]
-    ifuslot = mf[-14:-11]
-    filename = "multi_???_{}_???_{}.fits".format(ifuslot, amp)
-    s= "{}/virus/virus0000{}/{}/virus/{}".format(night, shotid, exp, filename)
-    #ff.append(filename)
-    path = os.path.join( basepath, s )
+per_amp_sky_spectra = []
 
-    _path = multifix(path)
-    #if _path != path:
-    #    print("Fixed path for multifits from {} to {}".format(path, _path))
-    path = _path
-
-    rebin_path = "rebin/{}v{}/{}".format(night, shotid, exp)
-    rebin_filename = filename.replace(".fits","_rebin.pickle")
-    rebin_file_path = os.path.join(rebin_path,rebin_filename)
-
-    if os.path.exists( rebin_file_path ) and not force_rebin:
-        # already rebinned?
-        with open( rebin_file_path , 'rb') as f:
-            print("Found previously rebinned data {}".format( rebin_file_path )) 
-            # The protocol version used is detected automatically, so we do not
-            # have to specify it.
-            #lw, rebinned = pickle.load(f, encoding='iso-8859-1')
-            lw, rebinned = pickle.load(f)
-            wlgrid = lw
-            per_amp_sky_spectra.append( np.nanmedian( rebinned['sky_spectrum']/rebinned['fiber_to_fiber'], axis=0 ) )
-            r['exists'] = True
-    else:
-        print("Read & rebin:", path)
-        if not os.path.exists( path ):
-            print("WARNING: Did not find {}".format(path))
+lw = None
+N = len(shotfiles)
+for i,shotfile in enumerate(shotfiles):
+    if i % 10 == 0:
+        print("Processing file number {} out of {}".format(i ,N))
+    #print("Processing {}".format( shotfile )) 
+    with open( shotfile , 'rb') as f:
+        _lw, rebinned = pickle.load(f)
+        # sanity check, make sure wlgrids are all the same
+        if type(lw) == type(None):
+            lw = _lw
         else:
-            hdu = fits.open(path)
-            lw, rebinned = get_rebinned(hdu, extensions, start = 3494.74, step =  1.9858398, stop = 5500.)
-            if type(wlgrid) == type(None):
-                wlgrid = lw
-            try:
-                os.makedirs(rebin_path)
-            except:
-                pass
-            with open(os.path.join(rebin_path,rebin_filename), 'wb') as f:
-                # Pickle the 'data' dictionary using the highest protocol available.
-                pickle.dump((lw, rebinned), f, pickle.HIGHEST_PROTOCOL)
+            if not (lw == _lw).all():
+                print("ERROR: Found Inconsistent wavelength grid in {}.".format(shotfile))
+        per_amp_sky_spectra.append( np.nanmedian( rebinned['{}sky_spectrum'.format(prefix)]/rebinned['fiber_to_fiber'], axis=0 ) )
 
-            per_amp_sky_spectra.append( np.nanmedian( rebinned['sky_spectrum']/rebinned['fiber_to_fiber'], axis=0 ) )
-            r['exists'] = True
 
-per_amp_sky_spectra = np.array(per_amp_sky_spectra)
-per_shot_sky_spectra = {}
-# now average all sky spectra for each shot 
-ut2 = table.unique(t, keys=['night', 'shotid'])
-ut1_ex = ut1[ut1['exists']]
-for r in ut2:
-    ii = ( ut1_ex['night'] == r['night']) * (ut1_ex['shotid'] == r['shotid'])
-    ff = np.nanmedian(per_amp_sky_spectra[ii],axis=0)
-    sout = Table([lw, ff], names=['wavelength', 'counts'], dtype=[float,float])
-    sout.write("{}v{}_sky.fits".format(r['night'], r['shotid']), format="fits", overwrite=True)
+# In[10]:
+
+
+global_sky = np.nanmedian(per_amp_sky_spectra,axis=0)
+sout = Table([lw, global_sky], names=['wavelength', 'counts'], dtype=[float,float])
+sout.write("{}_sky.fits".format(shot), format="fits", overwrite=True)
+
+
+# In[11]:
+
+
+if not COMMANDLINE:
+    from matplotlib import pyplot as plt
+    get_ipython().run_line_magic('matplotlib', 'inline')
+
+    f = plt.figure(figsize=[15,10])
+    plt.plot(lw, global_sky)
+    plt.xlabel("wavelength [A]")
+    plt.ylabel("flux [arb]")
+
+
+# In[ ]:
+
+
+
+
